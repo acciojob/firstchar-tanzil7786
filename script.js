0f160cd45e195d633735da3a53611a615dda0ba1
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-10 19:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstChar(text) {
   // your code here
+	for(let i=0;i<text.length;i++){
+		if(text[i]!==' '){
+			return text[i];
+		}
+	}
+	return '';
 }
 
 // Do not change the code below
